be2ee806ba26ce20d230dfb716a0071c3ceb3221
Omar Atcho <omar@Omars-MacBook-Pro.local>
2019-02-14 10:03:58 -0500

    adds all features except returning average

--- src/main/java/NumberSorterDemo.java
+++ src/main/java/NumberSorterDemo.java
@@ -1,13 +1,41 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class NumberSorterDemo {
 
     private int firstNumber;
     private int secondNumber;
     private int thirdNumber;
+    ArrayList<Integer> sortingArray = new ArrayList<Integer>();
 
 
-    public static void main(String[] args) {
-        
+    public void addItemsToArray(int firstNumber, int secondNumber, int thirdNumber){
+        sortingArray.add(firstNumber);
+        sortingArray.add(secondNumber);
+        sortingArray.add(thirdNumber);
+        }
+
+    public void returnLargestNumberInArray(){
+        Collections.max(sortingArray);
+    }
+
+    public void returnSmallestNumberInArray(){
+        Collections.min(sortingArray);
+    }
+
+    public void sortFromSmallestToLargest(){
+        Collections.sort(sortingArray);
+    }
+
+    public void sortFromLargestToSmallest(){
+        Collections.reverse(sortingArray);
+    }
+
+    public void returnAverageOfArrayListCollection(){
 
     }
+
+
+
 
 }
